ab51497427c0d24e3eac08d892d2c1ae13522fa5
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-26 13:58:58 +0530

    Initial commit

---
+++ src/Letcure1/Human.java
@@ -0,0 +1,52 @@
+package Letcure1;
+
+public class Human {
+    // Ascess Modifiers
+    // private - in this only the class itself can ascess the variable , function ....
+    // public - evey class can ascess this
+    // protected - every class in the same package can ascess them
+    // default - just a more linent version of protected , every class outside the package can also ascess
+    private String name ;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    private int height;
+    private int weight;
+    Human(String _name , int _age , int _height , int _weight){
+        name = _name;
+        age = _age;
+        height = _height;
+        weight = _weight;
+    }
+}
---
+++ src/Letcure1/RunnerClass.java
@@ -0,0 +1,23 @@
+package Letcure1;
+
+import java.util.Scanner;
+
+public class RunnerClass {
+    public static void main(String[] args) {
+        // keywords are the predefined words of a language that execute a certain task
+        Scanner s= new Scanner(System.in);
+        String name = s.nextLine();
+        int age = s.nextInt();
+        int height = s.nextInt();
+        int weight = s.nextInt();
+        Human human = new Human(name , age , height , weight);
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+        System.out.println(human.getHeight());
+        System.out.println(human.getWeight());
+
+        human.setName("Anjanay Raina");
+        System.out.println(human.getName());
+
+    }
+}
